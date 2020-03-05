b508083f9f9acee0ded5c6628b32342c521668e0
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-03-04 21:58:08 -0600

    reconfigured inquirer questions in do/while loop

--- Develop/app.js
+++ Develop/app.js
@@ -4,62 +4,113 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
-const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
-function promptUser() {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'manager',
-            message: 'Build Your engineering team. Who is the manager of your team?'
-        },
-        {
-            type: '',
-            name: '',
-            message: ''
-        },
-        {
-            type: '',
-            name: '',
-            message: ''
-        },
-        {
-            type: '',
-            name: '',
-            message: ''
-        },
-        {
-            type: '',
-            name: '',
-            message: ''
-        },
-    ])
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+let teamMembers = [];
+
+
+
+
+async function promptUser() {
+    do {
+
+        var response = await inquirer.prompt(
+            [
+
+                {
+                    type: 'list',
+                    name: 'role',
+                    message: 'Would you like to add a manager, engineer, or intern?',
+                    choices: [
+                        'Manager',
+                        'Engineer',
+                        'Intern',
+                        'Done adding team members'
+                    ]
+                },
+
+                {
+                    type: 'list',
+                    name: 'role',
+                    message: 'Would you like to add an engineer or intern?',
+                    choices: [
+                        'Engineer',
+                        'Intern',
+                        'Done adding team members'
+                    ],
+                    when: function (employees) {
+                        return employees.addAnotherEmployee;
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'name',
+                    message: 'What is the name of this team member?'
+                },
+                {
+                    type: 'input',
+                    name: 'email',
+                    message: "What is the team member's email?"
+                },
+                {
+                    type: 'input',
+                    name: 'id',
+                    message: "What is the team member's ID?"
+                },
+                {
+                    type: 'input',
+                    name: 'officeNumber',
+                    message: "What is the team member's office number?",
+                    when: function (employees) {
+                        return employees.role === 'Manager';
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'github',
+                    message: "What is the team member's gitlab username?",
+                    when: function (employees) {
+                        return employees.role === 'Engineer';
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'school',
+                    message: "What is the team member's school name?",
+                    when: function (employees) {
+                        return employees.role === 'Intern'
+                    }
+                },
+
+                {
+                    type: 'confirm',
+                    name: 'addEmployee',
+                    message: 'Would you like to add a team member?'
+                }
+
+            ]
+        )
+
+        if (response.role === 'Manager') {
+            const manager = new Manager(response.name, response.id, response.email, response.officeNumber);
+            teamMembers.push(manager)
+        }
+        else if (response.role === 'Engineer') {
+            const engineer = new Engineer(response.name, response.id, response.email, response.github);
+            teamMembers.push(engineer);
+        }
+        else if (response.role === 'Intern') {
+            const intern = new Intern(response.name, response.id, response.email, response.school);
+            teamMembers.push(intern);
+        }
+            console.log(teamMembers)
+
+
+    } while (response.addEmployee);
 }
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+
+promptUser()
